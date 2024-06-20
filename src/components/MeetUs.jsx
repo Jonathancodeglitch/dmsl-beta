45ac5066ded07229bc36c8f97a390469ceaf4b21
Jonathan <jonathankendrick697@gmail.com>
2024-06-20 16:39:10 -0700

    added about more page

---
+++ src/components/MeetUs.jsx
@@ -0,0 +1,40 @@
+export default function MeetUs() {
+  return (
+    <section className="meetUs container">
+      <h5>MEET US</h5>
+      <h2>Know Who We Are</h2>
+      <div className="team-container">
+        <div className="team">
+          <div className="profile"></div>
+          <div className="name">Jonathan Ohwevwo</div>
+          <div className="role">Web Developer</div>
+        </div>
+        <div className="team stepdown">
+          <div className="profile"></div>
+          <div className="name">Soji Fagade</div>
+          <div className="role">Director</div>
+        </div>
+        <div className="team">
+          <div className="profile"></div>
+          <div className="name">Jonathan Ohwevwo</div>
+          <div className="role">Web Developer</div>
+        </div>
+        <div className="team">
+          <div className="profile"></div>
+          <div className="name">Jonathan Ohwevwo</div>
+          <div className="role">Web Developer</div>
+        </div>
+        <div className="team stepdown">
+          <div className="profile"></div>
+          <div className="name">Jonathan Ohwevwo</div>
+          <div className="role">Web Developer</div>
+        </div>
+        <div className="team">
+          <div className="profile"></div>
+          <div className="name">Jonathan Ohwevwo</div>
+          <div className="role">Web Developer</div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/About-us.jsx
+++ src/pages/About-us.jsx
@@ -1,4 +1,26 @@
+import SecondaryHeroSection from "../components/SecondaryHeroSection";
 import Header from "../components/Header";
+import aboutUsHeroImg from "../img/about-us-hero-img.png";
+import MeetUs from "../components/MeetUs";
+import Footer from "../components/Footer";
+
 export default function AboutUsPage() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <SecondaryHeroSection
+        heroIntro="GET TO KNOW US"
+        heroBigText={
+          <>
+            DMSL, Where <span>Creativity </span>Meets Technology
+          </>
+        }
+        heroDesc="At DMSL, we recognise that at the heart of any solution lies STRATEGY, and that can only be acquired through use of accurate DATA. We believe that technology and digital transformation will continue to play a key role in creating solutions for sustainable business growth and profit for the 21st century business. “We, therefore, provide services and solutions to support and enable your business achieve a boost in customer acquisition, retention and growth in profit margins"
+        heroImg={aboutUsHeroImg}
+        name="about-us"
+      />
+      <MeetUs />
+      <Footer />
+    </>
+  );
 }
