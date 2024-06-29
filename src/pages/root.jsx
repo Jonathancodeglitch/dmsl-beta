e3d9486b6c08f0cd8016673585ba910cfddfd702
Jonathan <jonathankendrick697@gmail.com>
2024-06-29 04:27:55 -0700

    Added a component scroll to top to ensure the page scroll to the top when we navigate to a new route

--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/scrollToTop";
 import { Outlet } from "react-router-dom";
 
 export default function Root() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
