3d7ec3c1589cc5cb6e27a3779b41b2c4e5c3d4b5
Jonathan <jonathankendrick697@gmail.com>
2024-08-09 22:41:38 +0100

    debugging 001

--- server/index.js
+++ server/index.js
@@ -16,10 +16,6 @@ app.use(express.json());
 
 app.use(cors({ origin: "https://dmsl-beta-client.vercel.app" })); // Allow requests only from this domain
 
-app.get("/test_endpoint", (req, res) => {
-  res.json({ message: "Hello from API accessible by specific URL" });
-});
-
 const stripe = Stripe(process.env.STRIPE_PRIVATE_KEY);
 
 const offers = new Map([
@@ -30,7 +26,7 @@ const offers = new Map([
 
 app.post("/create-checkout-session", async (req, res) => {
   try {
-    const session = await stripe.checkout.sessions.create({
+    /*    const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
       line_items: req.body.packages.map((item) => {
@@ -50,7 +46,8 @@ app.post("/create-checkout-session", async (req, res) => {
       cancel_url: process.env.CLIENT_URL,
     });
 
-    res.json({ url: session.url });
+    res.json({ url: session.url }); */
+    res.json({ url: "url" });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
