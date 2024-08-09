1fbb49261d0cc2057efce2390a16e3a2f667d73d
Jonathan <jonathankendrick697@gmail.com>
2024-08-09 17:40:47 +0100

    Added backend

---
+++ client/src/components/Plan.jsx
@@ -0,0 +1,148 @@
+// integrate a payment system
+// the user should be able to click a package and re directed to the payment gate way where
+// he can input his card details to process payment
+
+// What i know ---======> NOTHING
+
+// what i need to know to implement
+// node js
+// stripe api
+// how to integrate the payment with react node js,stripe
+// api
+
+import { useState } from "react";
+import packages from "../planPackages.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+
+export default function Plan() {
+  return (
+    <section className="plan">
+      <div className="container row">
+        <div className="plan-content">
+          <h4>CHOOSE YOUR PLAN</h4>
+          <h1>Packages priced to suit your business</h1>
+          <ul className="plan-year">
+            <li>2022</li>
+            <li className="">2023</li>
+            <li className="active">2024</li>
+          </ul>
+          <p>
+            Our goal is to help you maximize value for you by starting from your
+            challenges all the way to your goals.
+          </p>
+        </div>
+        <PlanCards packages={packages} />
+      </div>
+    </section>
+  );
+}
+
+function PlanCards({ packages }) {
+  return (
+    <div className="plan-cards">
+      {packages.map((packages) => {
+        return (
+          <Card
+            key={packages.id}
+            id={packages.id}
+            packageName={packages.packageName}
+            description={packages.desc}
+            packageAmount={packages.amount}
+            services={packages.services}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function Card({ packageName, description, packageAmount, services, id }) {
+  return (
+    <div className="card">
+      <div className="card-title">{packageName}</div>
+      <div className="card-desc">{description}</div>
+      <div className="plan-amount">
+        {packageAmount}
+        <br />
+        <span>Monthly</span>
+      </div>
+      {/* services */}
+      <div className="service">
+        <div className="service_list-label">Services Included:</div>
+
+        {services.map((service, id) => {
+          return (
+            <ServiceListing
+              key={id}
+              serviceName={service.name}
+              serviceListing={service.list}
+            />
+          );
+        })}
+      </div>
+      <CheckOutButton id={id} />
+    </div>
+  );
+}
+
+function ServiceListing({ serviceName, serviceListing }) {
+  const [serviceList, setServiceList] = useState(false);
+
+  function handleServiceListClick() {
+    setServiceList(!serviceList);
+  }
+
+  return (
+    <div className="service_list">
+      <h3 className="service_list-title" onClick={handleServiceListClick}>
+        <FontAwesomeIcon icon={faCaretDown} /> {" " + serviceName + " "}
+      </h3>
+      {serviceList && (
+        <ul>
+          {serviceListing.map((serviceListing, id) => {
+            return <li key={id}>{serviceListing}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+function CheckOutButton({ id }) {
+  function handleClick(itemId) {
+    fetch("http://localhost:8000/create-checkout-session", {
+      method: "post",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        packages: [{ id: itemId, quantity: 1 }],
+      }),
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        //ELSE
+        return res.json().then((json) => {
+          Promise.reject(json);
+        });
+      })
+      .then(({ url }) => {
+        window.location = url;
+      })
+      .catch((e) => {
+        console.error(e.error);
+      });
+  }
+
+  return (
+    <button
+      onClick={() => {
+        handleClick(id);
+      }}
+      className="btn"
+    >
+      SELECT PACKAGE
+    </button>
+  );
+}
---
+++ server/server.js
@@ -0,0 +1,53 @@
+import dotenv from "dotenv";
+import express from "express";
+import Stripe from "stripe";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+const stripe = Stripe(process.env.STRIPE_PRIVATE_KEY);
+
+const offers = new Map([
+  [1, { name: "STARTER PACKAGE", price: 50000, quantity: 1 }],
+  [2, { name: "STANDARD PACKAGE", price: 100000, quantity: 1 }],
+  [3, { name: "PREMIUN PACKAGE", price: 200000, quantity: 1 }],
+]);
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.packages.map((item) => {
+        const offer = offers.get(item.id);
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: offer.name,
+            },
+            unit_amount: offer.price,
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: `${process.env.CLIENT_URL}`,
+      cancel_url: `${process.env.CLIENT_URL}`,
+    });
+
+    res.json({ url: session.url });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.listen(8000);
