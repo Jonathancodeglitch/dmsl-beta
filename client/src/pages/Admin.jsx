3a21d1fd23fa8d1c94db75dca0ae981f4a37e19a
Jonathan <jonathankendrick697@gmail.com>
2024-09-25 13:40:19 +0100

    removed local url for aweber

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,10 +1,7 @@
 export default function Admin() {
   //connect to my aweber accout
   return (
-    <form
-      action="https://dmsl-beta-xrq6.vercel.app/oauth2/authorize/oauth2"
-      method="get"
-    >
+    <form action="https://dmsl-beta-xrq6.vercel.app/oauth2" method="get">
       <button className="btn" style={{ marginBlock: "100px" }}>
         CONNECT TO AWEBER
       </button>
