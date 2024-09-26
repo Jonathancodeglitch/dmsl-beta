8e76b71e6bab024e2c6562e46aa2306eaab06a55
Jonathan <jonathankendrick697@gmail.com>
2024-09-26 16:10:54 +0100

    increased delay time

--- server/aweberApiCalls.js
+++ server/aweberApiCalls.js
@@ -166,7 +166,7 @@ async function handleNotifyingCustomersOnCanceledSubscription(subscriberEmail) {
       subscriberEmail
     );
 
-    await delay(20000);
+    await delay(5000);
     console.log("subscription has been canceled trigger but it was removed");
   }
 
