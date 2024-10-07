769b8756d7867a2a502165d28077a1701b3f139a
Jonathan <jonathankendrick697@gmail.com>
2024-10-07 11:09:30 +0100

    added promise

--- server/sendEmails.js
+++ server/sendEmails.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "send.one.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.email,
     pass: process.env.email_password,
@@ -14,12 +14,11 @@ const transporter = nodemailer.createTransport({
 });
 
 async function sendContactUsFormMessage(emailBody) {
-  try {
-    const info = await transporter.sendMail({
-      from: `"Dmsl" "${process.env.email}"`,
-      to: "jonathankendrick697@gmail.com",
-      subject: "Message from contact form",
-      html: `  <div style="max-width: 600px; margin: auto; padding: 20px; background-color: #ffffff; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); font-size:20px;">
+  const mailInfo = {
+    from: `"Dmsl" "${process.env.email}"`,
+    to: "jonathankendrick697@gmail.com",
+    subject: "Message from contact form",
+    html: `  <div style="max-width: 600px; margin: auto; padding: 20px; background-color: #ffffff; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); font-size:20px;">
               <img src="https://res.cloudinary.com/dv6uz0bks/image/upload/v1728264772/Logo-dark_zi7avz_tarasa.png" alt="dmsl logo" style="width: 150px;"/>
               
               <p style="color: #666;">Subject:<strong> ${emailBody.subject}</strong><br/> Message: ${emailBody.message} <br/> Sender email: ${emailBody.email}</p>
@@ -27,12 +26,19 @@ async function sendContactUsFormMessage(emailBody) {
                   &copy; 2024 Dmsl. All rights reserved.
               </div>
           </div>`, // html body
-    });
+  };
 
-    console.log("Message sent: %s", info.messageId);
-  } catch (error) {
-    console.log("message not sent", error);
-  }
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailInfo, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        resolve(info);
+        console.log("message sent");
+      }
+    });
+  });
 }
 
 export { sendContactUsFormMessage };
